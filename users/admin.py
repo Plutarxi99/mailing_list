241b0c697fb7c25a77e2554b4cce4744b155b982
Egor Shievanov <shievanov@bk.ru>
2023-11-19 15:35:40 +0300

    Добавил в admin панель поиск по email зарегестрированных пользователей

--- users/admin.py
+++ users/admin.py
@@ -3,13 +3,38 @@ from django.contrib import admin
 from users.models import User
 
 
+def get_user_by_email(email):
+    """
+    Надстройка для поиска по email пользователя
+    @param email:
+    @return:
+    """
+    try:
+        return User.objects.get(email=email)
+    finally:
+        return None
+
+
+class UserEmailSearchAdmin(admin.ModelAdmin):
+    def get_search_results(self, request, queryset, search_term):
+        user = get_user_by_email(search_term)
+        if user is not None:
+            queryset = queryset.filter(user_id=user.id)
+            use_distinct = False
+        else:
+            queryset, use_distinct = super().get_search_results(request,
+                                                                queryset,
+                                                                search_term)
+        return queryset, use_distinct
+
+
 @admin.register(User)
-class AdminGroupManager(admin.ModelAdmin):
+class AdminGroupManager(UserEmailSearchAdmin):
     list_display = ['email']
+    search_fields = ['email', ]
 
     def get_readonly_fields(self, request, obj=None):
-        print(request.user.groups.filter(name='Manager').exists())
-        if request.user.is_staff:  # when editing an object
+        if request.user.groups.filter(name='Manager').exists():  # when editing an object
             return ['email', 'first_name', 'last_name', 'avatar', 'phone', 'date_joined', 'last_login', 'groups',
                     'is_superuser', 'password', 'user_permissions']
         return self.readonly_fields
